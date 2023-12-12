71484db2d3a678d961f69c3296d928b9765c0bde
Rikesh Shakya <rickesh619@gmail.com>
2023-12-12 20:42:41 +0545

    Home Page UI Running

---
+++ app/src/main/java/com/smartherd/jewelleryshop/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.smartherd.jewelleryshop.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smartherd.jewelleryshop.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
