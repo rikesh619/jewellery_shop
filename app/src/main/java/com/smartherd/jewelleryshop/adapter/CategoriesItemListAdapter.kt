6b175c9e2b03db90c80eefd4cf9ab971dee7aed2
Rikesh Shakya <rickesh619@gmail.com>
2023-12-25 11:08:44 +0545

    Recycler View with Firebase loaded items Fixed

--- app/src/main/java/com/smartherd/jewelleryshop/adapter/CategoriesItemListAdapter.kt
+++ app/src/main/java/com/smartherd/jewelleryshop/adapter/CategoriesItemListAdapter.kt
@@ -1,6 +1,8 @@
 package com.smartherd.jewelleryshop.adapter
 
 import android.content.Context
+import android.text.TextUtils
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
@@ -30,19 +32,30 @@ class CategoriesItemListAdapter(val context: Context, val itemList: List<ItemCat
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val categoriesData = itemList[position]
+        val layoutWidth = 80
+        val layoutHeight = 80
+        val layout : ViewGroup.LayoutParams = holder.binding.itemCategoriesImg.layoutParams
 
-        val storageRef = Firebase.storage.reference.child("Categories/${categoriesData.image_name}.png")
+        val storageRef = Firebase.storage.reference.child("Categories/${categoriesData.image_name}")
 
         storageRef.downloadUrl.addOnSuccessListener {
 
             Glide.with(holder.itemView.context)
                 .load(it)
                 .into(holder.binding.itemCategoriesImg)
+
+            layout.height = layoutHeight
+            layout.width = layoutWidth
+
+            holder.binding.itemCategoriesImg.layoutParams = layout
+
         }.addOnFailureListener {
             Toast.makeText(holder.itemView.context , it.toString() , Toast.LENGTH_SHORT).show()
 
         }
+
         holder.binding.itemCategoriesName.text = categoriesData.name
+
 
     }
 
